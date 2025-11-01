3ea08ad66880222bae21961196c57bbf0260d5b7
lindseybarner <lindseybarner@gmail.com>
2025-11-01 01:00:30 -0400

    fixed menu toggle-to-scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,7 +66,7 @@ const overlay = document.querySelector("[data-overlay]");
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
-  document.body.classList.toggle("nav-active");
+  //document.body.classList.toggle("nav-active");
 }
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
